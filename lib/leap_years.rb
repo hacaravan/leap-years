05f2f1649c1e084d5a8437d80d7750900cf8abc0
Hugh Cavanagh <hacavanagh@gmail.com>
2020-12-28 13:43:13 +0000

    Wrote tests for divisible by 400 and divisible by 100 but not 400, passing these tests

--- lib/leap_years.rb
+++ lib/leap_years.rb
@@ -1,1 +1,3 @@
-"" 
+def leap_year(year)
+  year % 400 == 0
+end
--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -4,4 +4,7 @@ describe 'leap_years' do
   it 'returns true when a year divisible by 400 is passed' do
     expect(leap_year(2400)).to eq true
   end
+  it 'returns false when a year divisible by 100 but not by 400 is passed' do
+    expect(leap_year(2100)).to eq false
+  end
 end
