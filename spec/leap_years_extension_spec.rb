20f06e9c84fbd981681f62c8c78c168cf1530a5f
Hugh Cavanagh <hacavanagh@gmail.com>
2020-12-28 14:05:38 +0000

    Wrote test for short time frame

--- spec/leap_years_extension_spec.rb
+++ spec/leap_years_extension_spec.rb
@@ -4,4 +4,7 @@ describe 'leap_years_between' do
   it 'returns an empty array if there are no leap years between the years given' do
     expect(leap_years_between(2021, 2023)).to eq []
   end
+  it 'returns a single valued array for a simple period around a leap year' do
+    expect(leap_years_between(2019, 2021)).to eq [2020]
+  end
 end
