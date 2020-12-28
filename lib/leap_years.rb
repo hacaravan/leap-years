0a6a23c9513b91f990c26cbd5fc99c1f2322d21e
Hugh Cavanagh <hacavanagh@gmail.com>
2020-12-28 13:51:38 +0000

    Refactored

--- lib/leap_years.rb
+++ lib/leap_years.rb
@@ -1,6 +1,4 @@
 def leap_year(year)
-  if year % 4 == 0 && year % 100 == 0
-    return year % 400 == 0
-  else return year % 4 == 0
-  end
+  return year % 400 == 0 if year % 4 == 0 && year % 100 == 0
+  year % 4 == 0
 end
